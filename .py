4370f225c2edc6597d0cdb20058162c0d08d55cb
RishivardhanG <155731744+RishivardhanG@users.noreply.github.com>
2024-02-18 03:25:52 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,25 @@
+# lets make if, elif and else statement  example one
+# for this we wanna use the input function to get the input in the terminal which is run bar
+# we are using the integer value so we need to use the int for this
+# now lets start
+
+a = int(input("Enter the first number you want to know: "))  #you need to define the variable
+b = int(input("Enter the second number you want to know: "))
+
+if a==b:                                   #as we know that equals to symbol in python will be == so we need to give the cmd that if statment
+    print("Both are equal ")               # this statment will print only when the a is equal to b this is if statements
+
+elif a>b:                                  # we are using the elif statement  in the middle because at last we need to use the else statement so we need to use the elif first then else if we are not useing the elif directly we can use the else
+    print ("a is greater then b ")          # this statement will print only when a is greater then b
+
+else:                                      # For this we aer not going to define the specific one because already we have the two statements and there is only last one so we are using the else one
+    print ("b is greter than a ")
+
+
+#"""after running this program youc an see the input funciotn in the terminal now go to terminal and type what you want to konw any int then type enter again give the second number and then it will tell u which is greter which is equal
+# which is smaller """
+
+
+# note this is just a project to learn the if,else and elif statement and also we can use the multiple elif statements for this
+
+# i hope you understood,   If you have any doubts please mail me  : "rishivardhan.grv@gmail.com"
